2effc304ad9c8892f79f0c263c9717f94e953678
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-02-03 03:47:45 +0300

    Add project files.

---
+++ Portfoy/Controllers/AdminLayoutController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfoy.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        // GET: AdminLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _HeaderPartial() 
+        {
+            return PartialView();
+        }
+
+        public ActionResult _SidebarPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _Topbar()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
