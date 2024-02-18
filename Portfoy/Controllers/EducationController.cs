b90f178224629fa6c6a50a4009f3389a8a1887e4
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-02-18 05:59:10 +0300

    .

--- Portfoy/Controllers/EducationController.cs
+++ Portfoy/Controllers/EducationController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,5 +16,52 @@ namespace Portfoy.Controllers
             var values=db.TblEducation.ToList();    
             return View(values);
         }
+
+        [HttpGet]
+        public ActionResult CreateEducation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEducation(TblEducation e)
+        {
+            db.TblEducation.Add(e);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateEducation(int id)
+        {
+            var values=db.TblEducation.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEducation(TblEducation E)
+        {
+            db.TblEducation.Find(E.ID);
+            db.TblEducation.AddOrUpdate(E);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult DeleteEducation(int id)
+        {
+            var values = db.TblEducation.Find(id);
+            if (values!=null)
+            {
+                values.Durum = false;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
