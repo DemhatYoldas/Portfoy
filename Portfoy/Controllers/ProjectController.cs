59f127ca176d0f5994fbfe628f68d49fecb8e2f2
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2024-02-20 06:42:23 +0300

    .

--- Portfoy/Controllers/ProjectController.cs
+++ Portfoy/Controllers/ProjectController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,9 +17,48 @@ namespace Portfoy.Controllers
             return View(values);
         }
 
+        [HttpGet]
         public ActionResult CreateProject()
         {
+           
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProject(TblProject p)
+        {
+            db.TblProject.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+            var values = db.TblProject.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProject(TblProject P)
+        {
+            db.TblProject.AddOrUpdate(P);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult DeleteProject(int id)
+        {
+            var project = db.TblProject.Find(id);
+
+            if (project != null)
+            {
+                project.Durum = !project.Durum; // Durumun tersini alarak geçiş yap
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
